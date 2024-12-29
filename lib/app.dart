239d5b70de16391d5b60e191d27dda8558679f8e
Carlos Alexandre <carlos.alexandre.smo@gmail.com>
2024-12-29 19:49:01 -0300

    Add base structure

---
+++ lib/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_nasa_apod/src/core/common/constants/constants.dart';
+import 'package:flutter_nasa_apod/src/core/routes/app_router.dart';
+import 'package:get/get.dart';
+
+class FlutterNasaApod extends StatelessWidget {
+  const FlutterNasaApod({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp.router(
+      title: kAppName,
+      // theme: AppTheme.theme,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // scaffoldBackgroundColor: BackgroundColor.backgroundColor,
+        fontFamily: 'SFProDisplay',
+        // appBarTheme: Theme.of(context).appBarTheme.copyWith(
+        //   systemOverlayStyle: const SystemUiOverlayStyle(
+        //     // statusBarBrightness: Brightness.dark,
+        //     statusBarColor: Colors.amber
+        //   )
+        // ),
+      ),
+      key: GlobalKey<NavigatorState>(),
+      debugShowCheckedModeBanner: false,
+      routeInformationParser: AppRouter.router.routeInformationParser,
+      routerDelegate: AppRouter.router.routerDelegate,
+      routeInformationProvider: AppRouter.router.routeInformationProvider,
+      // navigatorObservers: [defaultLifecycleObserver],
+      // initialBinding: AppBinding(),
+    );
+  }
+}
