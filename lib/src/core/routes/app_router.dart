a7a1549f72523021ad1efb0345ae192d7584412b
Carlos Alexandre <carlos.alexandre.smo@gmail.com>
2025-01-03 16:40:40 -0300

    Add routes

--- lib/src/core/routes/app_router.dart
+++ lib/src/core/routes/app_router.dart
@@ -1,11 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_nasa_apod/src/core/routes/custom_transition.dart';
 import 'package:flutter_nasa_apod/src/core/routes/routes.dart';
 import 'package:flutter_nasa_apod/src/features/apod/data/datasources/local/favorite_data_source_impl.dart';
 import 'package:flutter_nasa_apod/src/features/apod/data/datasources/remote/apod_data_source.dart';
 import 'package:flutter_nasa_apod/src/features/apod/data/repositories/apod_repository_impl.dart';
 import 'package:flutter_nasa_apod/src/features/apod/presentation/controllers/apod_controller.dart';
+import 'package:flutter_nasa_apod/src/features/apod/presentation/controllers/favorites_controller.dart';
 import 'package:flutter_nasa_apod/src/features/apod/presentation/views/apod_page.dart';
+import 'package:flutter_nasa_apod/src/features/apod/presentation/views/favorites_page.dart';
 import 'package:flutter_nasa_apod/src/features/not_found_page.dart';
 import 'package:get/get.dart';
 import 'package:go_router/go_router.dart';
@@ -18,128 +21,36 @@ class AppRouter {
     initialLocation: AppRoutes.apod,
     debugLogDiagnostics: true,
     navigatorKey: _rootNavigatorKey,
-    // observers: [MyNavigatorObserver()],
     routes: [
-      // GoRoute(
-      //   name: AppRoutes.apod,
-      //   path: '/apod',
-      //   builder: (context, state) => const ApodPage(),
-      // ),
       GoRoute(
-        name: AppRoutes.apod,
-        path: '/apod',
-        builder: (context, state) => GetBuilder(
-          init: ApodController(ApodRepositoryImpl(
-              apodDataSource: ApodRemoteDataSourceImpl(Dio()),
-              favoriteDataSource: FavoriteDataSourceImpl())),
-          dispose: (state) {
-            Get.delete<ApodController>();
-          },
-          builder: (controller) => const ApodPage(),
-        ),
-      ),
-      // GoRoute(
-      //   name: AppRoutes.login,
-      //   path: '/login',
-      //   builder: (context, state) => GetBuilder(
-      //     init: LoginController(),
-      //     dispose: (state) {
-      //       Get.delete<LoginController>();
-      //     },
-      //     builder: (controller) => const LoginScreen(),
-      //   ),
-      // ),
-      // GoRoute(
-      //   //tarefas
-      //   name: AppRoutes.apod,
-      //   path: '/apod',
-      //   builder: (context, state) => GetBuilder(
-      //     init: ApodController(),
-      //     dispose: (state) {
-      //       Get.delete<ApodController>();
-      //     },
-      //     builder: (controller) => const ApodPage(),
-      //   ),
-      //   // routes: [
-      //   //   GoRoute(
-      //   //     name: AppRoutes.detail,
-      //   //     path: 'tarefa/:id',
-      //   //     pageBuilder: (_, state) {
-      //   //       final id = state.pathParameters['id']!;
-      //   //       return CustomSlideTransition(
-      //   //         key: state.pageKey,
-      //   //         child: GetBuilder(
-      //   //           init: DetailController(id: id),
-      //   //           dispose: (state) {
-      //   //             Get.delete<DetailController>();
-      //   //           },
-      //   //           builder: (controller) => const DetailScreen(),
-      //   //         ),
-      //   //       );
-      //   //     },
-      //   //     routes: [
-      //   //       GoRoute(
-      //   //         name: AppRoutes.services,
-      //   //         path: 'servicos',
-      //   //         pageBuilder: (_, state) {
-      //   //           return CustomSlideTransition(
-      //   //             child: GetBuilder(
-      //   //               init: ServicesController(),
-      //   //               dispose: (state) {
-      //   //                 Get.delete<ServicesController>();
-      //   //               },
-      //   //               builder: (controller) => const ServicesScreen(),
-      //   //             ),
-      //   //           );
-      //   //         },
-      //   //       ),
-      //   //       GoRoute(
-      //   //           name: AppRoutes.attendance,
-      //   //           path: 'atendimento',
-      //   //           pageBuilder: (_, state) {
-      //   //             return CustomSlideTransition(
-      //   //               child: GetBuilder(
-      //   //                 init: RegisterAttendanceController(),
-      //   //                 dispose: (state) {
-      //   //                   Get.delete<RegisterAttendanceController>();
-      //   //                 },
-      //   //                 builder: (controller) =>
-      //   //                     const RegisterAttendanceScreen(),
-      //   //               ),
-      //   //             );
-      //   //           },
-      //   //           routes: [
-      //   //             GoRoute(
-      //   //               name: AppRoutes.attachment,
-      //   //               path: 'anexo',
-      //   //               builder: (context, state) => GetBuilder(
-      //   //                 init: AttachmentController(),
-      //   //                 dispose: (state) {
-      //   //                   Get.delete<AttachmentController>();
-      //   //                 },
-      //   //                 builder: (controller) => const AttachmentScreen(),
-      //   //               ),
-      //   //             ),
-      //   //           ]),
-      //   //            GoRoute(
-      //   //         name: AppRoutes.shutdown,
-      //   //         path: 'fechamento',
-      //   //         pageBuilder: (_, state) {
-      //   //           return CustomSlideTransition(
-      //   //             child: GetBuilder(
-      //   //               init: ShutdownController(),
-      //   //               dispose: (state) {
-      //   //                 Get.delete<ShutdownController>();
-      //   //               },
-      //   //               builder: (controller) => const ShutdownScreen(),
-      //   //             ),
-      //   //           );
-      //   //         },
-      //   //       ),
-      //   //     ],
-      //   //   ),
-      //   // ]
-      // ),
+          name: AppRoutes.apod,
+          path: '/apod',
+          builder: (context, state) => GetBuilder(
+                init: ApodController(ApodRepositoryImpl(
+                    apodDataSource: ApodRemoteDataSourceImpl(Dio()),
+                    favoriteDataSource: FavoriteDataSourceImpl())),
+                dispose: (state) {
+                  Get.delete<ApodController>();
+                },
+                builder: (controller) => const ApodPage(),
+              ),
+          routes: [
+            GoRoute(
+              name: AppRoutes.favorites,
+              path: 'favorites',
+              pageBuilder: (_, state) {
+                return CustomSlideTransition(
+                  child: GetBuilder(
+                    init: FavoritesController(),
+                    dispose: (state) {
+                      Get.delete<FavoritesController>();
+                    },
+                    builder: (controller) => const FavoritesPage(),
+                  ),
+                );
+              },
+            ),
+          ]),
     ],
     errorBuilder: (context, state) => const NotFoundPage(),
   );
